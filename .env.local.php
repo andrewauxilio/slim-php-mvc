87b89c01687e754e6b1fc1254a133426e379419a
Andrew Auxilio <andrew.auxilio@geniussports.com>
2023-08-26 20:11:57 +1000

    remove local env

--- .env.local.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$_ENV['APP_ENV'] = 'dev';
-$_ENV['DB_HOST'] = 'localhost';
-$_ENV['DB_PORT'] = 3306;
-$_ENV['DB_NAME'] = 'express_js_example';
-$_ENV['DB_USER'] = 'andrewauxilio';
-$_ENV['DB_PASS'] = 'admin';
