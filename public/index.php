f26abcbf55dd1c2a5bbd7abbcd7eb91871d4e538
Andrew Auxilio <andrew.auxilio@geniussports.com>
2023-08-27 17:06:03 +1000

    clean up index.php

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 
 global $app;
 
-use Psr\Log\LoggerInterface;
-
 require __DIR__ . '/../vendor/autoload.php';
 
 /* Require Config */
@@ -13,24 +11,12 @@ require __DIR__ . '/../config/bootstrap.php';
 require __DIR__ . '/../.env.local.php';
 
 /* Require DI Container */
-require __DIR__ . '/../config/DI.php';
+require __DIR__ . '/../config/dependency-injection.php';
 
 /* Require Routes */
-require __DIR__ . '/../src/routes/api/ProductRoutes.php';
-require __DIR__ . '/../src/routes/api/AuthRoutes.php';
-require __DIR__ . '/../src/routes/admin/WebRoutes.php';
+require __DIR__ . '/../src/routes/routes.php';
 
-/**
- * Add Error Middleware
- *
- * @param bool                  $displayErrorDetails -> Should be set to false in production
- * @param bool                  $logErrors -> Parameter is passed to the default ErrorHandler
- * @param bool                  $logErrorDetails -> Display error details in error log
- * @param LoggerInterface|null  $logger -> Optional PSR-3 Logger
- *
- * Note: This middleware should be added last. It will not handle any exceptions/errors
- * for middleware added after it.
- */
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+/* Require Middlewares */
+require __DIR__ . '/../src/middlewares/middlewares.php';
 
 $app->run();
---
+++ src/routes/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * Gather all routes here. This file will be required in the public/index.php file.
+ */
+require __DIR__ . '/api/ProductRoutes.php';
+require __DIR__ . '/api/AuthRoutes.php';
+require __DIR__ . '/admin/WebRoutes.php';
